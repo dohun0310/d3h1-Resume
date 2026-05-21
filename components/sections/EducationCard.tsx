b70253ccadd4e55b57111ac90abbb7229c391860
김도훈 <dohun0310@naver.com>
2026-05-22 02:22:26 +0900

    [Feat/#8] Fix: 학력 카드 컴포넌트의 구분선 삽입 방식을 더 표준적인 방식으로 수정

--- components/sections/EducationCard.tsx
+++ components/sections/EducationCard.tsx
@@ -16,7 +16,7 @@ export default function EducationCard() {
       </h2>
       
       {education.map((edu, index) => (
-        <div key={index} className="flex flex-col gap-2 not-last:border-b last:border-none border-gray-100 pb-4">
+        <div key={index} className="flex flex-col gap-2 border-b last:border-none border-gray-100 pb-4">
           <div className="flex items-center gap-2">
             <GraduationCap size={20} aria-hidden="true" />
             <h3 className="text-lg font-semibold">{edu.school}</h3>
