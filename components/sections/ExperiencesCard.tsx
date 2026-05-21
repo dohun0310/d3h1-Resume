0bf25dba01a037a7feb2f92adbec6243e6f5dae9
김도훈 <dohun0310@naver.com>
2026-05-22 02:26:56 +0900

    [Feat/#9] Feat: 경험 카드 컴포넌트에서 경험 값이 없을 경우 null 반환 추가

--- components/sections/ExperiencesCard.tsx
+++ components/sections/ExperiencesCard.tsx
@@ -3,6 +3,10 @@ import { resume } from "@/data/resume";
 export default function ExperiencesCard() {
   const { experience } = resume;
 
+  if (experience.length === 0) {
+    return null;
+  }
+
   return (
     <></>
   );
