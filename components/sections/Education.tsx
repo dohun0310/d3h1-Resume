dce214bf3ad3ea8081cd28f0c80e908947175908
김도훈 <dohun0310@naver.com>
2026-05-22 02:00:10 +0900

    [Feat/#8] Feat: 학력 카드 컴포넌트 추가 및 학력 카드 구조 설정

---
+++ components/sections/Education.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+import { resume } from "@/data/resume";
+
+export default function EducationCard() {
+  const { education } = resume;
+
+  return (
+    <></>
+  );
+}
